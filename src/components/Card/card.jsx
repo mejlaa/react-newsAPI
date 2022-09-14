0f6a4a4af9ec0fceb17d38aacb202badbf8904cf
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2022-09-14 17:09:51 +0200

    jos style da uradim

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,14 +1,25 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { MyContext } from "../Context/context";
 import style from "./card.module.css";
 
 const CardNews = (props) => {
+  const { setImg, setTitle, setDesc, setAuthor } = useContext(MyContext);
+  const propsHandler = () => {
+    setImg(props.img);
+    setTitle(props.title);
+    setDesc(props.newsDesc);
+    setAuthor(props.author);
+  };
+
   return (
-    <div className={style.main}>
-      <div className={style.card}>
-        <img src={props.img} alt="img" />
-        <h3>{props.title}</h3>
-        <p>{props.newsDesc}</p>
-        <button>Read all</button>
-      </div>
+    <div className={style.card}>
+      <img src={props.img} alt="img" />
+      <h3>{props.title}</h3>
+      <p>{props.newsDesc}</p>
+      <button onClick={propsHandler}>
+        <Link to="/article">Read all</Link>
+      </button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ArticlePage from "./components/ArticlePage";
 import CardNews from "./components/Card/card";
+import Context from "./components/Context/context";
 import Main from "./components/fetch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <CardNews />
-  </React.StrictMode>
+  // <React.StrictMode>
+
+  <Context>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/article" element={<ArticlePage />} />
+      </Routes>
+    </BrowserRouter>
+  </Context>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
